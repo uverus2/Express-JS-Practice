f4ee2b708f6e24969a0c0be1d6f84daf786d7ab8
uverus2 <2ruzhk43@solent.ac.uk>
2019-08-22 23:08:40 +0100

    pug

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const express = require ('express');
+const express = require('express');
 
 // to create an express app call express
+
+// const colors = [
+//     'red',
+//     'orange',
+//     'yellow',
+//     'green',
+//     'blue',
+//     'purple'
+// ];
+
+const names = [{
+        firstName: "Kons",
+        lastName: "Ruzhev"
+    },
+    {
+        firstName: "Kons2",
+        lastName: "Ruzhev2"
+    },
+]
 
 const app = express();
 
@@ -13,9 +32,13 @@ app.get("/", (req, res) => {
 });
 
 app.get("/cards", (req, res) => {
-    res.render("card", {prompt: "Who is baried in the Grant's tomb", hint:"Think about whose tomb it is"});
+    res.render("card", { prompt: "Who is baried in the Grant's tomb", hint: "Think about whose tomb it is" });
+});
+
+app.get("/sandbox", (req, res) => {
+    res.render("sandbox", { names });
 });
 
 app.listen(3000, () => {
     console.log("Application is runnig on localhost:3000");
-});
+})
